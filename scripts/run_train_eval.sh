16829459ddb30062fb2662d38f92093cf8663dc0
bikman <bikman.m@gmail.com>
2024-02-03 14:34:07 +0200

    Update run_train_eval.sh

--- scripts/run_train_eval.sh
+++ scripts/run_train_eval.sh
@@ -2,7 +2,7 @@
 echo "Running ALL 39 proteins"
 for (( c=1; c<=39; c++ ))
 do
-        python3 run_full_flow.py -prot_set 1 -ep $c -model 1
+        python3 run_full_flow.py -ep $c
         sleep 2
 done
 echo "DONE!"
